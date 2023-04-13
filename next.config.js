b0012b53c2dc1c748acfe7fec6bad779b7838c0d
mrinalcs <mrinalvbu@gmail.com>
2023-04-13 08:10:03 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,2 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
-
+const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
