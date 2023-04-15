f590acc57e17f714524c0ad3df2efcfd2126746b
Mrinal <mrinalvbu@gmail.com>
2023-04-15 08:23:23 +0530

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Mrinal',
+    site_url: 'https://mrinal.tk',
+    feed_url: 'https://mrinal.tk/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
