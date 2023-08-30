19d0249ed9d412b3591068f7d6f007d3fb077927
Mrinal <mrinalvbu@gmail.com>
2023-08-30 21:45:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js');
+
+module.exports = withNextra({
+  async rewrites() {
+    return [
+      {
+        source: '/post/:slug*',
+        destination: '/posts/:slug*', // Redirect to the original /posts/:slug* route
+      },
+    ];
+  },
+});
